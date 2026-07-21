8860a5eb11472f9a5cf2a59ee5d812affdbed8bc
Amir Colbert <127263321+colbertamir@users.noreply.github.com>
2026-07-20 20:23:52 -0500

    completed executable, ran for testing

--- sort_img.py
+++ sort_img.py
@@ -5,3 +5,22 @@ folder = "Images"
 
 files = os.listdir(folder)
 
+for file in files:
+    if file.startswith("person"):
+        destination = os.path.join(folder, "Person")
+
+    elif file.startswith("animal"):
+        destination = os.path.join(folder, "Animal")
+
+    elif file.startswith("fruit"):
+        destination = os.path.join(folder, "Fruit")
+
+    else:
+        continue
+
+    os.makedirs(destination, exist_ok=True)
+
+    source = os.path.join(folder, file)
+
+    shutil.move(source, destination)
+
